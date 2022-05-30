d289403f82f1f703d8416924c94c93f9eb622299
Shineast <canfrog@naver.com>
2022-05-30 22:32:57 +0900

    Learn function of array in Numpy

---
+++ Numpy_Basic/01_Numpy_Array.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+a = np.arange(10)
+a
+
+b = np.array([10, 20, 30])
+b
+
+a.shape #array의 사이즈 반환
+a.ndim # dimention 차수 반환
+a.dtype # array의 데이터 타입 반환
+a.itemsize # array의 메모리 사용량 반환
+a.size # array의 값 개수 반환
+type(a) 
